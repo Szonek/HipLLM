cbc8d32d1796cc0176d3855f6139eee998db2e8b
szonek <szymonpl17@gmail.com>
2025-07-19 21:26:11 +0200

    implemeneted DataSet and DataLoaders

--- samples/BasicLLM/main.py
+++ samples/BasicLLM/main.py
@@ -1,10 +1,12 @@
 import urllib.request
 import torch
+from torch.utils.data import Dataset, DataLoader
 import os
 import re
+from importlib.metadata import version
+import tiktoken
 
-TOKEN_UNKNOWN = "<|unk|>"
-TOKEN_END_OF_TEXT = "<endoftext|>"
+print("tiktoken version: ", version("tiktoken"))
 
 def get_training_dataset():
     file_folder = "data"
@@ -25,59 +27,40 @@ print("Total number of characters: ", len(training_dataset))
 print(training_dataset[:99])
 
 
-def preprocess_text(input_str):
-    # Use regex to split on whitespace or any non-word character
-    split_str = re.split(r'(\s|--|[,.:;?_!"()\'])', input_str)
-    # Filter out empty strings (whitepsaces)
-    split_str = [item for item in split_str if item.strip()]
-    return split_str
-#print(preprocess_text("Hello, world. Is this-- a test?"))
-preprocessed_dataset = preprocess_text(training_dataset)
-print("Total number of preprocessed characters: ", len(preprocessed_dataset))
-print(preprocessed_dataset[:30])
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
 
-def get_vocab(input):
-    all_words = sorted(set(input))
-    all_words.extend([TOKEN_END_OF_TEXT, TOKEN_UNKNOWN])
-    vocab = {token:integer for integer,token in enumerate(all_words)}
-    return vocab
-
-vocab = get_vocab(preprocessed_dataset)
-vocab_size = len(vocab)    
-print("Vocab size: ", vocab_size)
-for i, item in enumerate(vocab.items()):
-    if i < 50 or i > vocab_size - 5:
-        print(item)
-
-class SimpleTokenizerV2:
-    def __init__(self, vocab):
-        self.str_to_int = vocab
-        self.int_to_str = {i:s for s,i in vocab.items()}
+        token_ids = tokenizer.encode(txt)
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i : i + max_length]
+            target_chunk = token_ids[i + 1 : i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
     
-    def encode(self, text):
-        # Use regex to split on whitespace or any non-word character
-        preprocessed = re.split(r'(\s|--|[,.:;?_!"()\'])', text)
-        # Filter out empty strings (whitepsaces)
-        preprocessed = [item.strip() for item in preprocessed if item.strip()]
-        # Replaces unknown words with "unknown" token
-        preprocessed = [item if item in self.str_to_int else TOKEN_UNKNOWN for item in preprocessed]
-        ids = [self.str_to_int[s] for s in preprocessed]
-        return ids
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+    
 
-    def decode(self, ids):
-        text = " ".join([self.int_to_str[i] for i in ids])
-        # Remove spaces before punctuation
-        text = re.sub(r'\s+([,.:;?_!"()\'])', r'\1', text)
-        return text
-        
+def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True,
+                         drop_last=True, num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset,                # Dataset object
+        batch_size=batch_size,  # Number of samples per batch
+        shuffle=shuffle,        # Whether to shuffle data each epoch
+        drop_last=drop_last,    # Whether to drop incomplete last batch
+        num_workers=num_workers # Number of parallel workers
+    )
+    return dataloader
 
-
-
-text1 = "Hello, do you like tea?"
-text2 = "In the sunlit terraces of the palace."
-text = (" " + TOKEN_END_OF_TEXT + " ").join([text1, text2])
-print(text)
-tokenizer = SimpleTokenizerV2(vocab)
-ids = tokenizer.encode(text)
-print(ids)
-print(tokenizer.decode(ids))
+dataloader = create_dataloader_v1(training_dataset, batch_size=1, max_length=4, stride=1, shuffle=False)
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
+print(next(data_iter))
