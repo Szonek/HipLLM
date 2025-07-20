f287fe83c8030b12762704a28637d557f838b053
szonek <szymonpl17@gmail.com>
2025-07-20 21:46:29 +0200

    simplified attention

--- samples/BasicLLM/custom_mha.py
+++ samples/BasicLLM/custom_mha.py
@@ -14,21 +14,31 @@ print("input shape:", inputs.shape)
 
 
 def simplified_attention(inputs):
-    # only 2nd query for visualisation purposes
-    query = inputs[1]
-    print("Query shape: ", query.shape)
-    attn_scores_2 = torch.empty(inputs.shape[0])
-    for i, x_in in enumerate(inputs):
-        attn_scores_2[i] = torch.dot(query, x_in)
-    print("Attn scores", attn_scores_2)
-    attn_weights_2_tmp = torch.softmax(attn_scores_2, dim=0)
-    print("Attn weights: ", attn_weights_2_tmp)
-    print("Attn weights sum: ", attn_weights_2_tmp.sum())
+    full_attn = True
+    if full_attn:
+        attn_scores = inputs @ inputs.T
+        print("Attn scores:\n", attn_scores)
+        attn_weights = torch.softmax(attn_scores, dim=-1)
+        print("Attn weights:\n", attn_weights)
+        print("Attn weights row sums:\n", attn_weights.sum(dim=-1))
+        context_vec = attn_weights @ inputs
+        print("Context vec:\n", context_vec)
+    else:
+        # only 2nd query for visualisation purposes
+        query = inputs[1]
+        print("Query shape: ", query.shape)
+        attn_scores_2 = torch.empty(inputs.shape[0])
+        for i, x_in in enumerate(inputs):
+            attn_scores_2[i] = torch.dot(query, x_in)
+        print("Attn scores", attn_scores_2)
+        attn_weights_2_tmp = torch.softmax(attn_scores_2, dim=0)
+        print("Attn weights: ", attn_weights_2_tmp)
+        print("Attn weights sum: ", attn_weights_2_tmp.sum())
 
-    context_vec_2 = torch.zeros(query.shape)
-    for i, x_in in enumerate(inputs):
-        context_vec_2 += attn_weights_2_tmp[i] * x_in
-    print("Context vec: ", context_vec_2)
+        context_vec_2 = torch.zeros(query.shape)
+        for i, x_in in enumerate(inputs):
+            context_vec_2 += attn_weights_2_tmp[i] * x_in
+        print("Context vec: ", context_vec_2)
 
 simplified_attention(inputs)
 
