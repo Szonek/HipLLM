529af8125f34343a75e213d7e8f85e2ac70cb954
szonek <szymonpl17@gmail.com>
2025-07-24 14:35:19 +0200

    placeholder model

--- samples/BasicLLM/model.py
+++ samples/BasicLLM/model.py
@@ -1,5 +1,5 @@
 import torch
-
+import torch.nn as nn
 
 GPT_CONFIG_124M = {
     "vocab_size":     50257,  # BPE tokenizer
@@ -10,3 +10,40 @@ GPT_CONFIG_124M = {
     "drop_rate":      0.1,    # 0.1 = 10%; used to prevent overfitting
     "qkv_bias":       False   # wheter to include bias in Linear layers of MHA
 }
+
+class DummyTransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        
+    def forward(self, x):
+        return x
+
+class DummyLayerNorm(nn.Module):
+    def __init__(self, normalized_shape, eps=1e-5):
+        super().__init__()
+        
+    def forward(self, x):
+        return x 
+
+class DummyGPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["embd_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        self.trf_blocks = nn.Sequential(
+            *[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])]
+        )
+        self.final_nom = DummyLayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
+
+    def forward(self, in_idx : torch.Tensor):
+        batch_size, seq_len = in_idx.shape
+        tok_emb = self.tok_emb(in_idx)
+        pos_emb = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_emb + pos_emb
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_nom(x)
+        logits = self.out_head(x)
+        return logits
